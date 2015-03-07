c0ce18fb6acbae2375ba993c510f5fe1c2872619
Jordan Georgiev <d3vildog@mail.bg>
2015-03-07 16:00:35 +0200

    Adding second problem BugFixLevel2

---
+++ Level-2/Advanced_JavaScript/Homework/2.SimulatingOOP/2.BugFixLevel2/BugFixLevel2.js
@@ -0,0 +1,45 @@
+﻿function Person(fname, lname) {
+    var firstName = fname;
+    var lastName = lname;
+    var fullName = firstName + ' ' + lastName;
+
+    var setFirstName = function(name) {
+        firstName = name;
+        fullName = firstName + ' ' + lastName;
+    }
+
+    var setLastName = function(name) {
+        lastName = name;
+        fullName = firstName + ' ' + lastName;
+    }
+
+    var setFullName = function(name) {
+        var arr = name.trim().split(/\s+/);
+        firstName = arr[0];
+        lastName = arr[1];
+        fullName = firstName + ' ' + lastName;
+    }
+    
+    setFirstName.toString = function () { return firstName + ""; }
+    setLastName.toString = function () { return lastName + ""; }
+    setFullName.toString = function () { return fullName + ""; }
+
+    return {
+        firstName: setFirstName,
+        lastName: setLastName,
+        fullName: setFullName
+    }
+}
+
+var person = new Person("Peter", "Jackson");
+// Getting values
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+// Changing values
+person.firstName = "Michael";
+console.log(person.firstName);
+//console.log(person.fullName);
+//person.lastName = "Williams";
+//console.log(person.lastName);
+//console.log(person.fullName);
